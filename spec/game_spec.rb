5283c2b86b9b9c748f0a924d709ca76133b26d80
saralein <sara.m.trujillo@gmail.com>
2017-08-10 16:19:09 -0500

    [Test] Add game class tests for updated roll method.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,17 +2,22 @@ require_relative '../lib/game.rb'
 
 describe Game do
   game = Game.new
-  for i in 0...20
-    game.roll(0)
+
+  def roll_many(n, pins, game)
+    for i in 0...n
+      game.roll(pins)
+    end
   end
-  puts game
+
   describe 'score' do
     it 'returns an integer' do
+      roll_many(20, 0, game)
       expect(game.score).to be_kind_of(Integer)
     end
 
-    it 'returns 0' do
-      expect(game.score).to eql(0)
+    it 'returns 20' do
+      roll_many(20, 1, game)
+      expect(game.score).to eql(20)
     end
   end
 end
