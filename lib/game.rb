e99abd8bf0a39b2628e6853134f7872c1813c7c2
saralein <sara.m.trujillo@gmail.com>
2017-08-10 16:58:46 -0500

    Add code to handle one spare.  Needs: refactor, to consider bounds.

--- lib/game.rb
+++ lib/game.rb
@@ -4,13 +4,24 @@
 class Game
   def initialize
     @score = 0
+    @rolls = Array.new(20){}
+    @current_roll = 0
   end
 
   def roll(pins)
-    @score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
 
   def score
+    for i in 0...20
+      @score += @rolls[i]
+      if(i.odd?)
+        if (@rolls[i - 1] + @rolls[i] == 10)
+          @score += @rolls[i + 1]
+        end
+      end
+    end
     return @score
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,23 +1,33 @@
 require_relative '../lib/game.rb'
 
 describe Game do
-  game = Game.new
+  before(:each) do
+    @game = Game.new
+  end
 
-  def roll_many(n, pins, game)
+  def roll_many(n, pins)
     for i in 0...n
-      game.roll(pins)
+      @game.roll(pins)
     end
   end
 
   describe 'score' do
-    it 'returns an integer' do
-      roll_many(20, 0, game)
-      expect(game.score).to be_kind_of(Integer)
+    it 'returns a score of 0' do
+      roll_many(20, 0)
+      expect(@game.score).to eql(0)
     end
 
-    it 'returns 20' do
-      roll_many(20, 1, game)
-      expect(game.score).to eql(20)
+    it 'returns a score of 20' do
+      roll_many(20, 1)
+      expect(@game.score).to eql(20)
+    end
+
+    it 'returns handles one spare' do
+      @game.roll(5)
+      @game.roll(5)
+      @game.roll(3)
+      roll_many(17, 0)
+      expect(@game.score).to eql(16)
     end
   end
 end
