6ae31c2d2fb6bab0c1fd498fdf0bd46a68b5617e
saralein <sara.m.trujillo@gmail.com>
2017-08-11 11:48:50 -0500

    Update tests for strikes and respective scoring.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,6 +11,40 @@ describe Game do
     end
   end
 
+  def roll_spare
+    @game.roll(5)
+    @game.roll(5)
+  end
+
+  def roll_strike
+    @game.roll(10)
+  end
+
+  describe 'is_spare' do
+    it 'returns true if frame is spare' do
+      roll_spare
+      expect(@game.is_spare(0)).to eql(true)
+    end
+
+    it 'returns false if frame is not spare' do
+      @game.roll(5)
+      @game.roll(4)
+      expect(@game.is_spare(0)).to eql(false)
+    end
+  end
+
+  describe 'is_strike' do
+    it 'returns true if frame is strike' do
+      roll_strike
+      expect(@game.is_strike(0)).to eql(true)
+    end
+
+    it 'returns false if frame is strike' do
+      roll_spare
+      expect(@game.is_strike(0)).to eql(false)
+    end
+  end
+
   describe 'score' do
     it 'returns a score of 0' do
       roll_many(20, 0)
@@ -22,12 +56,24 @@ describe Game do
       expect(@game.score).to eql(20)
     end
 
-    it 'returns handles one spare' do
-      @game.roll(5)
-      @game.roll(5)
+    it 'handles one spare' do
+      roll_spare
       @game.roll(3)
       roll_many(17, 0)
       expect(@game.score).to eql(16)
+    end
+
+    it 'handles one strike' do
+      @game.roll(10)
+      @game.roll(3)
+      @game.roll(4)
+      roll_many(16, 0)
+      expect(@game.score).to eql(24)
+    end
+
+    it 'correctly scores a perfect game' do
+      roll_many(12, 10)
+      expect(@game.score).to eql(300)
     end
   end
 end
